80cc88fa54e8bf549d04e614c3b479abf5fd5073
Alfonso Vidal <vidaltorres.alfonso@gmail.com>
2025-04-14 13:07:26 +0200

    Add fileURLToPath import and configure alias for src directory in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 import vercel from '@astrojs/vercel/serverless';
+import { fileURLToPath } from 'url';
 import icon from "astro-icon";
 
 // https://astro.build/config
@@ -9,5 +10,10 @@ export default defineConfig({  integrations: [icon()],
   output: 'server',
   adapter: vercel({}),
   vite: {
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
     plugins: [tailwindcss()],
   },});
